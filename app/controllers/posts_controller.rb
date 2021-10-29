bb44d4ce67948947fea821e410f72231e687dc2b
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-10-29 16:39:24 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index # indexに戻る
+    post = Post.create(content: params[:content])
+    render json:{ post: post}
   end
 end
